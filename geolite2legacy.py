4edf95c434c9c4220ead37272797479f154ae1b9
Sean Sweda <sweda@ibl.org>
2020-06-16 02:43:00 -0400

    US regions/states patch #27
    https://github.com/sherpya/geolite2legacy/issues/27

--- geolite2legacy.py
+++ geolite2legacy.py
@@ -245,7 +245,10 @@ class CityRev1RadixTree(RadixTree):
 
             nets = [ipaddr.IPNetwork(row['network'])]
             country_iso_code = location['country_iso_code'] or location['continent_code']
-            fips_code = geoname2fips.get(location['geoname_id'])
+            if location['subdivision_1_iso_code'] != "":
+                fips_code = location['subdivision_1_iso_code']
+            else:
+                fips_code = geoname2fips.get(location['geoname_id'])
             if fips_code is None:
                 logging.debug('Missing fips-10-4 for {}'.format(location['subdivision_1_name']))
                 fips_code = '00'
